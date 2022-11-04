fc743189db7d837ad7f95816b685504dc905faaf
rattleSSnake <rattleSSnake@protonmail.com>
2022-11-04 16:09:52 +0100

    Update example.py

--- example.py
+++ example.py
@@ -6,7 +6,8 @@ class App:
         self.x = 0
         pyxel.run(self.update, self.draw)
 
-    def update(self): self.x = (self.x + 1) % pyxel.width
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
 
     def draw(self):
         pyxel.cls(0)
