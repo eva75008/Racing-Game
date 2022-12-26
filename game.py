b1bb60fef5609532bdbe5cb04d787918bf379838
rattleSSnake <rattleSSnake@protonmail.com>
2022-12-26 15:06:17 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,5 @@
 import pyxel
 
-difference = 8
-
 
 class Game:
     def __init__(self):
@@ -71,37 +69,76 @@ class Road:
 
 class Milestones(Road):
     def __init__(self):
-        self.height = 35
-        self.width = 8
+        self.min_height = 8
+        self.min_width = 2
+        self.scaling_step = 1.5
         self.start = 10
         super().__init__()
 
     def update(self):
-        # make the milestones move
         pass
 
-    def right(self):
-        x = pyxel.width - self.start - difference
-        while x > 433:
-            y = (
-                (x - self.convergent_point[0] + self.width)
-                / (pyxel.width + self.overflow_x - self.convergent_point[0])
-            ) * (pyxel.height - self.convergent_point[1]) + self.convergent_point[1]
+    def findSlope(self, x0, y0):
+        return (y0 - self.convergent_point[1]) / (x0 - self.convergent_point[0])
 
-            pyxel.rect(x, y - self.height, self.width, self.height, pyxel.COLOR_RED)
-            pyxel.rect(x, y - self.height + 4, 8, 5, pyxel.COLOR_WHITE)
-            x -= 25
+    def findDistance(self, x0, slope):
+        return abs(((slope**2 + 1) ** (1 / 2)) * (x0 - self.convergent_point[0]))
+
+    def findX(self, y0, slope):
+        return (y0 - self.convergent_point[1]) / slope + self.convergent_point[0]
+
+    def right(self):
+        slope = self.findSlope(pyxel.width - self.start, 375)
+        total_distance = self.findDistance(self.start, slope)
+        cper_dist = self.findDistance(self.findX(self.end_road, slope), slope)
+        scaling_factor = 1
+        distance = cper_dist
+        while distance < total_distance:
+            x = (distance / ((slope**2 + 1) ** (1 / 2))) + self.convergent_point[0]
+            y = slope * (x - self.convergent_point[0]) + self.convergent_point[1]
+            x_align_correction = self.min_width * scaling_factor
+            pyxel.rect(
+                x - x_align_correction,
+                y - self.min_height * scaling_factor,
+                self.min_width * scaling_factor,
+                self.min_height * scaling_factor,
+                pyxel.COLOR_RED,
+            )
+            pyxel.rect(
+                x - x_align_correction,
+                y - (self.min_height - 1) * scaling_factor,
+                self.min_width * scaling_factor,
+                scaling_factor,
+                pyxel.COLOR_WHITE,
+            )
+            distance += ((total_distance - cper_dist) / 8) * scaling_factor
+            scaling_factor += self.scaling_step
 
     def left(self):
-        x = self.start
-        while x < 280:
-            y = (
-                (x - self.convergent_point[0])
-                / (-self.overflow_x - self.convergent_point[0])
-            ) * (pyxel.height - self.convergent_point[1]) + self.convergent_point[1]
-            pyxel.rect(x, y - self.height, self.width, self.height, pyxel.COLOR_RED)
-            pyxel.rect(x, y - self.height + 4, 8, 5, pyxel.COLOR_WHITE)
-            x += 25
+        slope = self.findSlope(self.start, 375)
+        total_distance = self.findDistance(self.start, slope)
+        cper_dist = self.findDistance(self.findX(self.end_road, slope), slope)
+        scaling_factor = 1
+        distance = cper_dist
+        while distance < total_distance:
+            x = (-distance / ((slope**2 + 1) ** (1 / 2))) + self.convergent_point[0]
+            y = slope * (x - self.convergent_point[0]) + self.convergent_point[1]
+            pyxel.rect(
+                x,
+                y - self.min_height * scaling_factor,
+                self.min_width * scaling_factor,
+                self.min_height * scaling_factor,
+                pyxel.COLOR_RED,
+            )
+            pyxel.rect(
+                x,
+                y - (self.min_height - 1) * scaling_factor,
+                self.min_width * scaling_factor,
+                scaling_factor,
+                pyxel.COLOR_WHITE,
+            )
+            distance += ((total_distance - cper_dist) / 8) * scaling_factor
+            scaling_factor += self.scaling_step
 
     def draw(self):
         self.right()
