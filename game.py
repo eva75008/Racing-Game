8b9162677600b101eac9cf7504d444a2db531185
eva75008 <117632330+eva75008@users.noreply.github.com>
2023-01-16 09:07:43 +0100

    Update game.py

--- game.py
+++ game.py
@@ -18,7 +18,7 @@ class Game:
         self.road.draw()
         self.milestone.draw()
         
-        pyxel.blt(50, 50, 0, 0, 16, 31, 31, 12)
+        pyxel.blt(x=100, y=100, img=1, u=7, v=0, w=10, h=40, colkey=0)
 
 class Road:
     def __init__(self):
