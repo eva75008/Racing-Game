aadb161427b0d7e46f1a728844e34a50affb7e7b
rattleSSnake <rattleSSnake@protonmail.com>
2023-03-29 21:42:50 +0200

    Update open_editor.py

--- open_editor.py
+++ open_editor.py
@@ -1,17 +1,16 @@
-import tkinter.filedialog as fd
-import subprocess
-import sys
+from tkinter.filedialog import askopenfilename
+from subprocess import run
+from sys import path
 
 # Open a file dialog to select a .pyxres file
-file_path = fd.askopenfilename(
-    initialdir=sys.path[0],
-    title="Select a Pyxel resource file",
+file_path = askopenfilename(
+    initialdir=path[0],
+    title="Select a Pyxel Resource File",
     filetypes=[("Pyxel Resource Files", "*.pyxres")],
 )
 
 # If a file was selected, open it in the Pyxel Editor
 if file_path:
-    subprocess.run(["python", "-m", "pyxel", "edit", file_path], check=True)
+    run(["python", "-m", "pyxel", "edit", file_path], check=True)
 else:
-    print("No .pyxres file selected.")
-    sys.exit(1)
+    exit("No .pyxres file selected.")
