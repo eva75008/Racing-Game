ea5f6c0c2df16764dbd85c9390d907d95fd607ca
eva75008 <117632330+eva75008@users.noreply.github.com>
2023-05-22 08:05:05 +0200

    Update essais_milestones.py

--- essais_milestones.py
+++ essais_milestones.py
@@ -186,6 +186,7 @@ class TimeOfDay:
             pyxel.COLOR_BLACK,
         )
 
+        
 
 class Milestones:
     def __init__(self, Xf):
@@ -241,79 +242,8 @@ class Milestones:
             )
 
 
-
-
-
-
-
-
-
-
-
-
-class Milestone:
-    def __init__(self):
-        self.min_height = 8
-        self.min_width = 2
-        self.scaling_step = 1.5
-        self.start = 10
-
-    def update(self):
-        pass
-
-    def findSlope(self, x0, y0):
-        return (y0 - convergence[1]) / (x0 - convergence[0])
-
-    def findDistance(self, x0, slope):
-        return abs(sqrt(slope**2 + 1) * (x0 - convergence[0]))
-
-    def findX(self, y0, slope):
-        return (y0 - convergence[1]) / slope + convergence[0]
-
-    def drawLine(self, start_x, slope, is_right):
-        total_distance = self.findDistance(self.start, slope)
-        cper_dist = self.findDistance(self.findX(sky_height, slope), slope)
-        scaling_factor = 1
-        distance = cper_dist
-        while distance < total_distance:
-            if is_right:
-                x = distance / (slope**2 + 1) ** (1 / 2) + convergence[0]
-            else:
-                x = -distance / (slope**2 + 1) ** (1 / 2) + convergence[0]
-            y = slope * (x - convergence[0]) + convergence[1]
-            if is_right:
-                x_align_correction = self.min_width * scaling_factor
-            else:
-                x_align_correction = 0
-            pyxel.rect(
-                x - x_align_correction,
-                y - self.min_height * scaling_factor,
-                self.min_width * scaling_factor,
-                self.min_height * scaling_factor,
-                pyxel.COLOR_RED,
-            )
-            pyxel.rect(
-                x - x_align_correction,
-                y - (self.min_height - 1) * scaling_factor,
-                self.min_width * scaling_factor,
-                scaling_factor,
-                pyxel.COLOR_WHITE,
-            )
-            distance += (total_distance - cper_dist) / 8 * scaling_factor
-            scaling_factor += self.scaling_step
-
-    def right(self):
-        slope = self.findSlope(screen_width - self.start, 375)
-        self.drawLine(screen_width - self.start, slope, True)
-
-    def left(self):
-        slope = self.findSlope(self.start, 375)
-        self.drawLine(self.start, slope, False)
-
-    def draw(self):
-        self.right()
-        self.left()
-
+            
+            
 
 class Player:
     def __init__(self):
