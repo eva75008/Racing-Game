c7d1c40fff528d853d5ad951a414fb03c720cde8
rattleSSnake <rattleSSnake@protonmail.com>
2023-05-24 13:53:05 +0200

    update

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
 import pyxel
 from math import sin, radians, sqrt
-from random import randint
+from random import uniform
 
 
 screen_width = 720
 screen_height = 480
-convergence = (screen_width // 2, 200)
-sky_height = screen_height // 2
+convergence = (screen_width / 2, 200)
+sky_height = screen_height / 2
 
 
 class Game:
@@ -14,121 +14,39 @@ class Game:
         pyxel.init(screen_width, screen_height, "Retro-racing Game", 60)
         pyxel.load("graphic.pyxres")
         self.road = Road()
-        self.milestonel = Milestones(0)
-        self.milestoner = Milestones(720)
+        self.milestone = Milestones()
         self.timeOfDay = TimeOfDay()
-        self.player = Player()
+        self.score = 0
+        self.player = Player(self)
+        self.enemy_timer = 0
         self.enemies = Enemies(self.player)
+        pyxel.playm(0, loop=True)
         pyxel.run(self.update, self.draw)
 
+    def init_score(self):
+        self.score = 0
+
     def update(self):
+        self.score += 0.1
         self.timeOfDay.update()
-        self.milestonel.update()
-        self.milestoner.update()
         self.player.update()
         self.enemies.update()
+        self.enemy_timer += 1
+        if self.enemy_timer >= 20:
+            self.enemies.create_enemy()
+            self.enemy_timer = 0
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_LIME)
         self.road.draw()
         self.enemies.draw()
         self.timeOfDay.draw()
-        self.milestonel.draw()
-        self.milestoner.draw()
+        self.milestone.draw()
         self.player.draw()
+        pyxel.text(10, 10, f"Score: {int(self.score)}", pyxel.COLOR_WHITE)
 
 
-        
 class Road:
-    def __init__(self):
-        self.Xo = screen_width // 2
-        self.Yo = 200
-        self.max_height = 64
-        self.Xg = screen_width / 3
-        self.Yg = screen_height
-        self.Xd = (screen_width * 2) / 3
-        self.Yd = screen_height
-        self.l = self.Xd- self.Xg
-        #self.sizes_coeff = self.min_width
-        self.sizes_coeff = 18
-
-        self.startx = self.Xo
-        self.starty = self.Yo
-         #self.Xf = self.Xg
-        
-        self.overflowX = 200
-        self.line_thickness = 40
-        
-        self.endY = self.starty
-
-
-    def update(self):
-        pass
-
-    
-    def createRoad(self):
-        pyxel.tri(
-            -self.overflowX,
-            screen_height,
-            screen_width + self.overflowX,
-            screen_height,
-            self.Xo,
-            self.Yo,
-            pyxel.COLOR_GRAY,
-        )
-    
-    def outlineRoad(self):
-        for n in range(2):
-            for x in range(self.line_thickness):
-                pyxel.line(
-                    (-self.overflowX if n else screen_width + self.overflowX),
-                    screen_height + x,
-                    convergence[0],
-                    convergence[1],
-                    pyxel.COLOR_WHITE,
-                )
-    
-    def directions(self, Xf):
-        self.Xf = Xf
-        
-        if self.starty > screen_height +50:
-            self.__init__()
-
-        self.coeff = (self.Yg-self.Yo)/(self.Xf-self.Xo)
-
-        self.size = self.sizes_coeff*(abs(self.startx - self.Xo) / (screen_width/2))
-
-        self.starty += 7 * ((self.starty-199)/(screen_height-200))
-        self.startx = (self.starty-200)/self.coeff + self.Xo
-        
-        self.endY = self.starty + self.size
-        self.endX = (self.endY - 200)/self.coeff + self.Xo
-
-
-    def draw(self):
-        self.createRoad()
-        self.outlineRoad()
-
-        self.directions(self.Xg)
-        for n in range(1, 3):
-            for x in range(self.line_thickness):
-                pyxel.line(
-                    self.startx,
-                    self.starty,
-                    self.Xo - self.size,
-                    self.Yo * self.coeff,
-                    #self.endX + x,
-                    #self.endY,
-                    pyxel.COLOR_WHITE,
-                )
-            self.directions(self.Xd)
-
-
-        
-        
-        
-        
-class Roads:
     def __init__(self):
         self.overflowX = 200
         self.line_thickness = 40
@@ -156,20 +74,6 @@ class Roads:
                 )
 
     def createLanes(self):
-        """for x in range(self.line_thickness):
-            line(
-                x1,
-                y1,
-                x2,
-                y2,
-                pyxel.COLOR_WHITE,
-            )
-"""
-
-
-
-
-
         for n in range(1, 3):
             for x in range(self.line_thickness):
                 pyxel.line(
@@ -188,35 +92,41 @@ class Roads:
 
 
 class Enemies:
-    def __init__(self, player, scaling_factor=0.05):
+    def __init__(self, player, number=3):
         self.player = player
-        (self.x, self.y) = convergence
-        self.scaling_factor = scaling_factor
-        self.trajectory = randint(-1, 1)
+        self.enemies = []
+        self.scaling_factor = 0.05
 
-    def calculateRadius(self, y, initial_size):
+    def calculate_radius(self, y, initial_size):
         y -= convergence[1]
         return self.scaling_factor * y + initial_size
 
-    def increaseSpeed(self, y, initial_speed):
+    def increase_speed(self, y, initial_speed):
         y -= convergence[1] - 1
         return initial_speed * 2 ** (y / 60)
 
+    def create_enemy(self):
+        enemy = {
+            "x": convergence[0],
+            "y": convergence[1],
+            "trajectory": uniform(1.6, -1.6),
+        }
+        self.enemies.append(enemy)
+
     def update(self):
-        self.y += self.increaseSpeed(self.y, 1)
-        self.x += self.trajectory * self.increaseSpeed(self.y, 1)
-        if self.y > screen_height + self.calculateRadius(self.y, 8):
-            self.y = convergence[1]
-            self.x = convergence[0]
-            self.trajectory = randint(-1, 1)
+        for enemy in self.enemies:
+            enemy["y"] += self.increase_speed(enemy["y"], 1)
+            enemy["x"] += enemy["trajectory"] * self.increase_speed(enemy["y"], 1)
+            if enemy["y"] > screen_height + self.calculate_radius(enemy["y"], 8):
+                self.enemies.remove(enemy)
 
-        # Check collision with player
-        if self.checkCollision():
-            self.player.onCollision()
+            # Check collision with player
+            if self.check_collision(enemy):
+                self.player.onCollision()
 
-    def checkCollision(self):
+    def check_collision(self, enemy):
         # Calculate the radius of the ball used in the collision detection
-        radius = self.calculateRadius(self.y, 8)
+        radius = self.calculate_radius(enemy["y"], 8)
 
         # Calculate the dimensions of the player rectangle used in the collision detection
         x = self.player.x + self.player.width * 0.3
@@ -226,14 +136,20 @@ class Enemies:
 
         # Check if the ball collides with the player
         return (
-            self.x + radius >= x
-            and self.x - radius <= x + width
-            and self.y + radius >= y
-            and self.y - radius <= y + height
+            enemy["x"] + radius >= x
+            and enemy["x"] - radius <= x + width
+            and enemy["y"] + radius >= y
+            and enemy["y"] - radius <= y + height
         )
 
     def draw(self):
-        pyxel.circ(self.x, self.y, self.calculateRadius(self.y, 8), pyxel.COLOR_WHITE)
+        for enemy in self.enemies:
+            pyxel.circ(
+                enemy["x"],
+                enemy["y"],
+                self.calculate_radius(enemy["y"], 8),
+                pyxel.COLOR_WHITE,
+            )
 
 
 class TimeOfDay:
@@ -276,67 +192,71 @@ class TimeOfDay:
             pyxel.COLOR_BLACK,
         )
 
-        
 
 class Milestones:
-    def __init__(self, Xf):
-        self.Xo = screen_width // 2
-        self.Yo = 200
-        self.max_height = 64
-        self.Xg = 0
-        self.Yg = screen_height - 150
-        self.Xd = 720
-        self.Yd = screen_height - 150
-        self.l = self.Xd- self.Xg
-        #self.sizes_coeff = self.min_width
-        self.sizes_coeff = 18
+    def __init__(self):
+        self.min_height = 8
+        self.min_width = 2
+        self.scaling_step = 1.5
+        self.start = 10
 
-        self.startx = self.Xo
-        self.starty = self.Yo
-        self.Xf = Xf
+    def findSlope(self, x0, y0):
+        return (y0 - convergence[1]) / (x0 - convergence[0])
 
+    def findDistance(self, x0, slope):
+        return abs(sqrt(slope**2 + 1) * (x0 - convergence[0]))
 
-    def update(self):
-        pass
+    def findX(self, y0, slope):
+        return (y0 - convergence[1]) / slope + convergence[0]
 
-    def directions(self):
-        if self.starty >= screen_height-100:
-            self.__init__(self.Xf)
-
-        self.coeff = (self.Yg-self.Yo)/(self.Xf-self.Xo)
-
-        self.size = self.sizes_coeff*(abs(self.startx - self.Xo) / (screen_width/2))
-
-        self.starty += 7 * ((self.starty-199)/(screen_height-200))
-        self.startx = (self.starty-200)/self.coeff + self.Xo - (30)
-
-
-    def draw(self):
-        self.directions()
-
-        if self.starty > 220:
+    def drawLine(self, start_x, slope, is_right):
+        total_distance = self.findDistance(self.start, slope)
+        cper_dist = self.findDistance(self.findX(sky_height, slope), slope)
+        scaling_factor = 1
+        distance = cper_dist
+        while distance < total_distance:
+            if is_right:
+                x = distance / (slope**2 + 1) ** (1 / 2) + convergence[0]
+            else:
+                x = -distance / (slope**2 + 1) ** (1 / 2) + convergence[0]
+            y = slope * (x - convergence[0]) + convergence[1]
+            if is_right:
+                x_align_correction = self.min_width * scaling_factor
+            else:
+                x_align_correction = 0
             pyxel.rect(
-                    self.startx,
-                    self.starty,
-                    self.size,
-                    self.size*4,
-                    pyxel.COLOR_RED,
-                )
-
+                x - x_align_correction,
+                y - self.min_height * scaling_factor,
+                self.min_width * scaling_factor,
+                self.min_height * scaling_factor,
+                pyxel.COLOR_RED,
+            )
             pyxel.rect(
-                self.startx,
-                self.starty + self.size * 0.45,  #.45 ==> size delta Y / size
-                self.size,
-                self.size * 0.6,
+                x - x_align_correction,
+                y - (self.min_height - 1) * scaling_factor,
+                self.min_width * scaling_factor,
+                scaling_factor,
                 pyxel.COLOR_WHITE,
             )
+            distance += (total_distance - cper_dist) / 8 * scaling_factor
+            scaling_factor += self.scaling_step
 
+    def right(self):
+        slope = self.findSlope(screen_width - self.start, 375)
+        self.drawLine(screen_width - self.start, slope, True)
 
-            
-            
+    def left(self):
+        slope = self.findSlope(self.start, 375)
+        self.drawLine(self.start, slope, False)
+
+    def draw(self):
+        self.right()
+        self.left()
+
 
 class Player:
-    def __init__(self):
+    def __init__(self, game):
+        self.game = game
         self.width = 95
         self.height = 150
         self.player_speed = 10
@@ -346,14 +266,16 @@ class Player:
         self.y = screen_height - self.height
 
     def update(self):
-        if pyxel.btn(pyxel.KEY_LEFT) and self.x > 5:
+        if (pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.KEY_Q)) and self.x > 5:
             self.x -= self.player_speed
-        if pyxel.btn(pyxel.KEY_RIGHT) and self.x < screen_width - self.width - 5:
+        if (
+            pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.KEY_D)
+        ) and self.x < screen_width - self.width - 5:
             self.x += self.player_speed
 
     def onCollision(self):
-        None
-        #pyxel.quit()   #animation de fin de jeu
+        pyxel.play(2, 2)
+        self.game.init_score()
 
     def draw(self):
         # motorcycle
