4c91dd59af7dfa1ba53cfe041ee2f6e65fa2a3f1
eva75008 <117632330+eva75008@users.noreply.github.com>
2023-05-24 09:36:07 +0200

    Update jeeeeu.py

--- jeeeeu.py
+++ jeeeeu.py
@@ -273,6 +273,7 @@ class Player:
             self.x += self.player_speed
 
     def onCollision(self):
+        pyxel.quit()
         while not pyxel.btn(KEY_SPACE):
             pyxel.cls(8)
             text(320, 210, "Click on SPACE to RESTART", 7)
